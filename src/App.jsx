dbf9585415c0a582f41d682a17397789e828e8fb
kareem24 <108769715+Kareem24@users.noreply.github.com>
2024-04-27 21:47:37 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button';
+import './api/api';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-700">hello world</h1>
+      <Button> shadcn button</Button>
+    </div>
+  );
+}
+
+export default App;
