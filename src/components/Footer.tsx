0ad8a3765892023314a8c748ff0591f7e23e4133
kareem roqib <kareemolakunleh@gmail.com>
2024-05-18 10:12:01 +0100

    chore: setup and structure the footer section of the website

---
+++ src/components/Footer.tsx
@@ -0,0 +1,66 @@
+import { product } from '@/lib/product';
+import { Link } from 'react-router-dom';
+import facebook from '../assets/images/facebook-link.png';
+import twitter from '../assets/images/twitter-link.png';
+import instagram from '../assets/images/instagram-link.png';
+import linkedin from '../assets/images/linkedin-link.png';
+import Logo from './Logo';
+
+const Footer = () => {
+  const { title, children } = product;
+  return (
+    <footer>
+      <div className="grid grid-cols-2 bg-fino-green-90 px-4 py-6 text-white md:px-[7.5rem] md:py-10">
+        <div>
+          <p className="mb-6 font-Urbanist text-[2rem] font-semibold md:mb-12">
+            {title}
+          </p>
+          <ul className="space-y-6 *:font-Urbanist *:text-xl *:capitalize md:space-y-12">
+            {children.map((item) => {
+              return (
+                <li key={item.title}>
+                  <Link to={item.link}>{item.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <p className="mb-6 font-Urbanist text-[2rem] font-semibold capitalize md:mb-12">
+            contact us{' '}
+          </p>
+
+          <div className="space-y-6 *:font-Urbanist *:text-xl *:capitalize md:space-y-12">
+            <p>
+              <span className="font-medium">contact :</span>{' '}
+              <span>+012345678</span>
+            </p>
+            <p>
+              <span className="font-medium">email :</span>{' '}
+              <span>support@finowise.com</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 px-4  py-8 text-white md:px-[7.5rem] ">
+        <Logo />
+        <ul className="flex items-center gap-4 *:*:w-8 *:cursor-pointer ">
+          <li>
+            <img src={facebook} alt="" width={24} height={24} />
+          </li>
+          <li>
+            <img src={instagram} alt="" width={24} height={24} />
+          </li>
+          <li>
+            <img src={twitter} alt="" width={24} height={24} />
+          </li>
+          <li>
+            <img src={linkedin} alt="" width={24} height={24} />
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/types/product.types.ts
@@ -0,0 +1,8 @@
+export type NavItems = {
+  title: string;
+  children: NavChildren[];
+};
+export type NavChildren = {
+  title: string;
+  link: string;
+};
