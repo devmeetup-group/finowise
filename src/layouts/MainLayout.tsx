cfa39fa7bb215119777ad100c539f9e71ed9d658
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-05-21 22:06:49 +0100

    moved footer component to mainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import Header from '@/components/reusables/Header';
 import { Outlet } from 'react-router-dom';
 
@@ -6,7 +7,7 @@ const MainLayout = () => {
     <>
       <Header />
       <Outlet />
-      {/* -----ADD FOOTER -------- */}
+      <Footer />
     </>
   );
 };
